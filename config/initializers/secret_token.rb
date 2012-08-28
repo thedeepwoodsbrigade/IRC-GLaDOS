6bc71c382f0a73d5b49d711e6a6fffdaf4d0208b
LordbyronIII <bryan@thedeepwoodsbrigade.com>
2012-08-28 19:31:50 -0400

    just setting up the app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IRCGlados::Application.config.secret_token = '6c7f7f7a94fe5b5557b0140fab69512a4a79ad3a1a4c6be1afeeb59ca4a9d1df6a0a574b70a15b86e5d2a5452e2865863904ce9d1a43ed6542a758c9d66180eb'
