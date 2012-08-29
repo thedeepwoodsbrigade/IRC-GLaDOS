e9eb76fee8de0e6b387f61ea9730e15e8ad3f06c
LordbyronIII <bryan@thedeepwoodsbrigade.com>
2012-08-28 20:46:31 -0400

    cant figure out why rails s fails

--- Gemfile
+++ Gemfile
@@ -1,17 +1,48 @@
 source 'https://rubygems.org'
 
-gem 'rails', '3.2.8'
-gem 'mongoid', '3.0.0.rc'
-gem 'rails3-generators'
-gem 'pry'
-gem 'pry-nav'
-gem 'cinch'
-
 group :assets do
   gem 'sass-rails',   '~> 3.2.3'
   gem 'coffee-rails', '~> 3.2.1'
   gem 'uglifier', '>= 1.0.3'
 end
 
+gem 'rails', '3.2.8'
+gem 'mongoid', '3.0.0.rc'
+gem 'rails3-generators'
+gem 'pry'
+gem 'pry-nav'
+gem 'cinch'
+gem 'execjs'
+gem 'moped'
 gem 'jquery-rails'
+gem 'therubyracer'
+gem 'coffee-filter'
+gem 'haml_coffee_assets'
 
+group :production do
+  gem 'thin'
+end
+
+group :development do
+  gem 'heroku'
+  gem 'taps'
+  gem 'thin'
+end
+
+group :test, :development do
+  gem 'factory_girl_rails'
+  gem 'rspec-rails'
+  gem 'capybara'
+  gem 'launchy'
+  gem 'mongoid-rspec'
+  gem 'timecop'
+  gem 'vcr'
+  gem 'fakeweb'
+  gem 'email_spec' 
+  gem 'rack_session_access'
+  gem 'pry'
+  gem 'pry-nav'
+
+  # Pretty printed test output
+  gem 'turn', require: false
+end
--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,8 @@
-require File.expand_path('../boot', __FILE__)
-
 require 'rails/all'
+
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "active_resource/railtie"
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
@@ -8,9 +10,26 @@ if defined?(Bundler)
   # If you want your assets lazily compiled in production, use this line
   # Bundler.require(:default, :assets, Rails.env)
 end
-
-module IRCGlados
+module IRCGLados
   class Application < Rails::Application
+
+    # Generators to automatically generate using non-rails standard gems
+    config.generators do |g|
+      g.orm                 :mongoid
+      g.template_engine     :haml
+      g.test_framework      :rspec
+      g.fixture_replacement :factory_girl
+
+      g.view_specs          false
+      g.helper_specs        false
+      g.controller_specs    false
+    end
+
+    config.hamlcoffee.escapeAttributes = false
+    config.hamlcoffee.escapeHtml = false
+
+    config.middleware.use RackSessionAccess::Middleware if Rails.env.test?
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -38,20 +57,6 @@ module IRCGlados
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
-
-    # Enable escaping HTML in JSON.
-    config.active_support.escape_html_entities_in_json = true
-
-    # Use SQL instead of Active Record's schema dumper when creating the database.
-    # This is necessary if your schema can't be completely dumped by the schema dumper,
-    # like if you have constraints or database-specific column types
-    # config.active_record.schema_format = :sql
-
-    # Enforce whitelist mode for mass assignment.
-    # This will create an empty whitelist of attributes available for mass-assignment for all models
-    # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
-    # parameters by using an attr_accessible or attr_protected declaration.
-    config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
     config.assets.enabled = true
